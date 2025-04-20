d74d17ce66c1dfe1001338978987af60cf0d287a
AshoShibu <ashoshibu@gmail.com>
2025-04-20 19:23:11 +0530

    Update StudentNotFoundException.java

--- StudentNotFoundException.java
+++ StudentNotFoundException.java
@@ -1,1 +1,8 @@
+// Thrown when a student with the specified ID is not found.
+
+public class StudentNotFoundException extends Exception {
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
 
